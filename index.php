54662319d96b9cc4a3b26510c1171e028c077378
amailtoamal <amalchacko95@gmail.com>
2018-08-20 18:09:31 +0530

    7

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 
 
     echo "Hello world!<br>Im Amal<br>Whats Up..";
-hfsfgdgfds
+
+$connection = new MongoClient('mongodb://user:pass@ds051605.mlab.com:51605/db');
+if($connection)
+    echo "true";
+else
+    echo "fasle";
 
 
 ?>
